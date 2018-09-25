173075e87234372fbb5ea1ea9edd4aee636e2e25
William Baker <wilbaker@microsoft.com>
2018-09-25 15:38:36 -0700

    More functional test work

--- GVFS/GVFS.FunctionalTests/Tests/EnlistmentPerFixture/SymbolicLinkTests.cs
+++ GVFS/GVFS.FunctionalTests/Tests/EnlistmentPerFixture/SymbolicLinkTests.cs
@@ -1,4 +1,8 @@
-﻿using GVFS.FunctionalTests.FileSystemRunners;
+﻿using System.IO;
+using GVFS.FunctionalTests.FileSystemRunners;
+using GVFS.FunctionalTests.Should;
+using GVFS.FunctionalTests.Tools;
+using GVFS.Tests.Should;
 using NUnit.Framework;
 
 namespace GVFS.FunctionalTests.Tests.EnlistmentPerFixture
@@ -8,15 +12,45 @@ namespace GVFS.FunctionalTests.Tests.EnlistmentPerFixture
     [TestFixtureSource(typeof(FileSystemRunner), FileSystemRunner.TestRunners)]
     public class SymbolicLinkTests : TestsWithEnlistmentPerFixture
     {
-        private FileSystemRunner fileSystem;
-        public SymbolicLinkTests(FileSystemRunner fileSystem)
+        private const string TestFolderName = "Test_EPF_SymbolicLinks";
+
+        private const string TestFileName = "TestFile.txt";
+        private const string TestFileContents = "This is a real file";
+        private const string TestFile2Name = "TestFile2.txt";
+        private const string TestFile2Contents = "This is the second real file";
+        private const string ChildFolderName = "ChildDir";
+        private const string ChildLinkName = "LinkToFileInFolder";
+        private const string GrandChildLinkName = "LinkToFileInParentFolder";
+
+        private BashRunner bashRunner;
+        public SymbolicLinkTests()
         {
-            this.fileSystem = fileSystem;
+            this.bashRunner = new BashRunner();
         }
 
         [TestCase, Order(1)]
         public void CheckoutBranchWithSymLinks()
         {
+            GitHelpers.CheckGitCommandAgainstGVFSRepo(
+                this.Enlistment.RepoRoot, 
+                "checkout FunctionalTests/20180925_SymLinksPart1",
+                "Switched to branch 'FunctionalTests/20180925_SymLinksPart1'");
+
+            string testFilePath = this.Enlistment.GetVirtualPathTo(Path.Combine(TestFolderName, TestFileName));
+            testFilePath.ShouldBeAFile(this.bashRunner).WithContents(TestFileContents);
+            this.bashRunner.IsSymbolicLink(testFilePath).ShouldBeFalse($"{testFilePath} should not be a symlink");
+
+            string testFile2Path = this.Enlistment.GetVirtualPathTo(Path.Combine(TestFolderName, TestFile2Name));
+            testFile2Path.ShouldBeAFile(this.bashRunner).WithContents(TestFile2Contents);
+            this.bashRunner.IsSymbolicLink(testFile2Path).ShouldBeFalse($"{testFile2Path} should not be a symlink");
+
+            string childLinkPath = this.Enlistment.GetVirtualPathTo(Path.Combine(TestFolderName, ChildLinkName));
+            this.bashRunner.IsSymbolicLink(childLinkPath).ShouldBeTrue($"{childLinkPath} should be a symlink");
+            childLinkPath.ShouldBeAFile(this.bashRunner).WithContents(TestFileContents);
+
+            string grandChildLinkPath = this.Enlistment.GetVirtualPathTo(Path.Combine(TestFolderName, ChildFolderName, GrandChildLinkName));
+            this.bashRunner.IsSymbolicLink(grandChildLinkPath).ShouldBeTrue($"{grandChildLinkPath} should be a symlink");
+            grandChildLinkPath.ShouldBeAFile(this.bashRunner).WithContents(TestFile2Contents);
         }
 
         [TestCase, Order(2)]
