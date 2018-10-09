ea11bb9639021120c68b23735892d531de69bf4e
William Baker <wilbaker@microsoft.com>
2018-10-09 07:53:31 -0700

    Add comments

--- GVFS/GVFS.FunctionalTests/Categories.cs
+++ GVFS/GVFS.FunctionalTests/Categories.cs
@@ -14,8 +14,13 @@
             // The FailsOnBuildAgent category is for tests that pass on dev
             // machines but not on the build agents
             public const string FailsOnBuildAgent = "FailsOnBuildAgent";
+
+            // Tests that require the LockHolder project to be converted to .NET Core (#150)
             public const string NeedsLockHolder = "NeedsDotCoreLockHolder";
+
+            // Tests that require #356 (old paths to be delivered with rename notifications)
             public const string NeedsRenameOldPath = "NeedsRenameOldPath";
+
             public const string M3 = "M3_AllGitCommands";
             public const string M4 = "M4_All";
         }
