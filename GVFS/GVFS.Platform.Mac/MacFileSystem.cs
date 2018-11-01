0b5669028c931f8db7b37aac51aa67c3842686ec
William Baker <wilbaker@microsoft.com>
2018-11-01 16:14:34 -0700

    Complete stat work

--- GVFS/GVFS.Platform.Mac/MacFileSystem.cs
+++ GVFS/GVFS.Platform.Mac/MacFileSystem.cs
@@ -1,5 +1,6 @@
 ﻿using GVFS.Common;
 using GVFS.Common.FileSystem;
+using System;
 using System.IO;
 using System.Runtime.InteropServices;
 
@@ -45,10 +46,10 @@ namespace GVFS.Platform.Mac
 
         public bool IsExecutable(string fileName)
         {
-            StatBuffer statBuffer = new StatBuffer();
-            if (Stat(fileName, out statBuffer) == 0)
+            NativeStat.StatBuffer statBuffer = new NativeStat.StatBuffer();
+            if (NativeStat.Stat(fileName, out statBuffer) == 0)
             {
-                return true;
+                return NativeStat.IsExecutable(statBuffer.Mode);
             }
 
             return false;
@@ -56,10 +57,10 @@ namespace GVFS.Platform.Mac
 
         public bool IsSocket(string fileName)
         {
-            StatBuffer statBuffer = new StatBuffer();
-            if (Stat(fileName, out statBuffer) == 0)
+            NativeStat.StatBuffer statBuffer = new NativeStat.StatBuffer();
+            if (NativeStat.Stat(fileName, out statBuffer) == 0)
             {
-                return true;
+                return NativeStat.IsSock(statBuffer.Mode);
             }
 
             return false;
@@ -71,41 +72,70 @@ namespace GVFS.Platform.Mac
         [DllImport("libc", EntryPoint = "rename", SetLastError = true)]
         private static extern int Rename(string oldPath, string newPath);
 
-        [DllImport("libc", EntryPoint = "stat", SetLastError = true)]
-        private static extern int Stat(string path, [Out] out StatBuffer statBuffer);
-
-        [StructLayout(LayoutKind.Sequential)]
-        private struct TimeSpec
+        private static class NativeStat
         {
-            public long Sec;
-            public long Nsec;
-        }
+            // #define  S_IFMT      0170000     /* [XSI] type of file mask */
+            private static readonly ushort IFMT = Convert.ToUInt16("170000", 8);
 
-        [StructLayout(LayoutKind.Sequential)]
-        private struct StatBuffer
-        {
-            public int Dev;              /* ID of device containing file */
-            public ushort Mode;          /* Mode of file (see below) */
-            public ushort NLink;         /* Number of hard links */
-            public ulong Ino;            /* File serial number */
-            public uint UID;             /* User ID of the file */
-            public uint GID;             /* Group ID of the file */
-            public int RDev;             /* Device ID */
+            // #define  S_IFSOCK    0140000     /* [XSI] socket */
+            private static readonly ushort IFSOCK = Convert.ToUInt16("0140000", 8);
 
-            public TimeSpec ATimespec;     /* time of last access */
-            public TimeSpec MTimespec;     /* time of last data modification */
-            public TimeSpec CTimespec;     /* time of last status change */
-            public TimeSpec BirthTimespec; /* time of file creation(birth) */
+            // #define S_IXUSR     0000100     /* [XSI] X for owner */
+            private static readonly ushort IXUSR = Convert.ToUInt16("100", 8);
 
-            public long Size;          /* file size, in bytes */
-            public long Blocks;        /* blocks allocated for file */
-            public int BlkSize;        /* optimal blocksize for I/O */
-            public uint Glags;         /* user defined flags for file */
-            public uint Gen;           /* file generation number */
-            public int LSpare;         /* RESERVED: DO NOT USE! */
+            // #define S_IXGRP     0000010     /* [XSI] X for group */
+            private static readonly ushort IXGRP = Convert.ToUInt16("10", 8);
 
-            [MarshalAs(UnmanagedType.ByValArray, SizeConst = 2)]
-            public long[] QSpare;     /* RESERVED: DO NOT USE! */
+            // #define S_IXOTH     0000001     /* [XSI] X for other */
+            private static readonly ushort IXOTH = Convert.ToUInt16("1", 8);
+
+            public static bool IsSock(ushort mode)
+            {
+                // #define  S_ISSOCK(m) (((m) & S_IFMT) == S_IFSOCK)    /* socket */
+                return (mode & (IXUSR | IXGRP | IXOTH)) != 0;
+            }
+
+            public static bool IsExecutable(ushort mode)
+            {
+                return (mode & IFMT) == IFSOCK;
+            }
+
+            [DllImport("libc", EntryPoint = "stat", SetLastError = true)]
+            public static extern int Stat(string path, [Out] out StatBuffer statBuffer);
+
+            [StructLayout(LayoutKind.Sequential)]
+            public struct TimeSpec
+            {
+                public long Sec;
+                public long Nsec;
+            }
+
+            [StructLayout(LayoutKind.Sequential)]
+            public struct StatBuffer
+            {
+                public int Dev;              /* ID of device containing file */
+                public ushort Mode;          /* Mode of file (see below) */
+                public ushort NLink;         /* Number of hard links */
+                public ulong Ino;            /* File serial number */
+                public uint UID;             /* User ID of the file */
+                public uint GID;             /* Group ID of the file */
+                public int RDev;             /* Device ID */
+
+                public TimeSpec ATimespec;     /* time of last access */
+                public TimeSpec MTimespec;     /* time of last data modification */
+                public TimeSpec CTimespec;     /* time of last status change */
+                public TimeSpec BirthTimespec; /* time of file creation(birth) */
+
+                public long Size;          /* file size, in bytes */
+                public long Blocks;        /* blocks allocated for file */
+                public int BlkSize;        /* optimal blocksize for I/O */
+                public uint Glags;         /* user defined flags for file */
+                public uint Gen;           /* file generation number */
+                public int LSpare;         /* RESERVED: DO NOT USE! */
+
+                [MarshalAs(UnmanagedType.ByValArray, SizeConst = 2)]
+                public long[] QSpare;     /* RESERVED: DO NOT USE! */
+            }
         }
 
         private static class NativeFileReader
