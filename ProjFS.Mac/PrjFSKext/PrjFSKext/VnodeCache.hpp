3eef1321ef6f89f06f846b65adebd6243059d8d7
William Baker <wilbaker@microsoft.com>
2019-01-03 06:42:01 -0800

    Add remaining members to VnodeCacheEntry

--- ProjFS.Mac/PrjFSKext/PrjFSKext/VnodeCache.hpp
+++ ProjFS.Mac/PrjFSKext/PrjFSKext/VnodeCache.hpp
@@ -15,6 +15,9 @@ private:
     struct VnodeCacheEntry
     {
         vnode_t vnode;
+        uint32_t vid;   // vnode generation number
+        uint16_t vrgid; // virtualization root generation number
+        VirtualizationRootHandle virtualizationRoot;
     };
     
     VnodeCacheEntry* entries;
